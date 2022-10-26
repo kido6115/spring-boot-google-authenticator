96c4d3cbd65f2d2b74e49470bf31f0bd9b3b52f8
sungyeh <sungyeh@tist.com.tw>
2022-10-26 09:43:36 +0800

    init

---
+++ src/main/java/com/sungyeh/SpringBootGoogleAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.sungyeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleAuthenticatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sungyeh/SpringBootGoogleAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungyeh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
